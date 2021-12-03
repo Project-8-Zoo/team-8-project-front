9bbbd8b68d8307ffa2156c89dab2565a2b4571ae
Jonathan Singh <jmsingh9899@gmail.com>
2021-12-03 13:49:02 -0800

    shit

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../ui/Navbar.css';
 
 function Navbar() {
+    const [count, setCount] = useState(5)
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (count == 0) {
+                console.log("done")
+                clearInterval(interval)
+            } else {
+                setCount(count - 1)
+                console.log(count)
+            }
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [count])
     return (
         <body className="NavContainer">
             <div className="Menu">
-            <h1>Menu</h1>
+                <h1>Menu</h1>
             </div>
             <div className="Score">
-            <h1>Score</h1>
+                <h1>Score</h1>
             </div>
             <div className="Time">
-            <h1>Time</h1>
+                <h1>{count}</h1>
             </div>
             <div className="HiScore">
-            <h1>High Score</h1>
+                <h1>High Score</h1>
             </div>
         </body>
     )
